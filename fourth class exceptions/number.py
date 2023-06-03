60ef675fc8f519be5c7e65266b9e9c9f1b878a4a
Ratkovski <drezcry@hotmail.com>
2023-06-03 20:57:01 -0300

    Exceptions

---
+++ fourth class exceptions/number.py
@@ -0,0 +1,35 @@
+##1
+# def main():
+#     x = get_int()
+#     print(f"x is {x}")
+#
+# def get_int():
+#     while True:
+#         try:
+#             x = int(input("What's x? "))
+#         except ValueError:
+#             print("x is not an integer")
+#         else:
+#             break
+#     return x
+#
+#
+# main()
+
+
+##2
+
+def main():
+    x = get_int()
+    print(f"x is {x}")
+
+def get_int():
+    while True:
+        try:
+            return int(input("What's x? "))
+        except ValueError:
+            pass
+
+
+main()
+
